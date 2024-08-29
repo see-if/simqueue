a1baacdf43b386d686eb7d3f486307df3008d03c
ryan <2259601853@qq.com>
2024-08-29 18:29:57 +0800

    ADD code

--- README.md
+++ README.md
@@ -1,3 +1,9 @@
 # simqueue
 
 纯go实现的异步延时队列
+
+可以直接引入
+
+```bash
+go get github.com/see-if/simqueue
+```
--- queue_test.go
+++ queue_test.go
@@ -6,9 +6,16 @@ import (
 )
 
 func TestQueue(t *testing.T) {
+	//获取实例
 	qe := GetEntry()
+	//加入延时队列
 	qe.Schedule(func() {
 		println("hi!")
 	}, 3*time.Second, "id_001")
+	if time.Now().Year() == 2014 {
+		//取消队列
+		qe.Cancel("id_001")
+	}
+	//保持运行
 	qe.Run()
 }
